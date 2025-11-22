8d0f1393bdeeeea45ff077b07ff2d280d180d047
trejorodrigo862-ctrl <trejorodrigo862@gmail.com>
2025-11-21 21:02:52 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
-const rootElement = document.getElementById("root");
+mport React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AppProvider } from "./context/AppContext";
 
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
       <App />
