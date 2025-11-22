d5d0351269a22ad0d945b33b74dd90e397e47f6d
trejorodrigo862-ctrl <trejorodrigo862@gmail.com>
2025-11-22 09:54:25 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 👈 AGREGAR ESTA LÍNEA CLAVE
+      base: './', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
