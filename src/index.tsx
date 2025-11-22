c5a04a3fb9e13b491cc2e3fbad96c5bd6f0dd62c
trejorodrigo862-ctrl <trejorodrigo862@gmail.com>
2025-11-22 12:24:57 -0300

    Fix import statement for App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx'; // ¡Importación con ruta relativa corregida!
+import App from './App.tsx'; 
 import { AppProvider } from '@/context/AppContext';
 import '@/index.css';
 
@@ -8,7 +8,6 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
