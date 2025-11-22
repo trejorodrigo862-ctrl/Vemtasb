f33b54a063e28167d38a7732f0a718b37d78eb0e
trejorodrigo862-ctrl <trejorodrigo862@gmail.com>
2025-11-22 11:44:10 -0300

    Change App import to include file extension

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '@/App';
+import App from '@/App.tsx';
 import { AppProvider } from '@/context/AppContext';
 import '@/index.css';
 
